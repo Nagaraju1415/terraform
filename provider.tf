3305efbeb9e672fc84b05684b1806ef619f5c51c
Nagaraju1415 <120045380+Nagaraju1415@users.noreply.github.com>
2023-03-05 16:08:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -33,3 +33,21 @@ sytemctl start jenkins
 systemctl start jenkins
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+-----------------------------------
+  amazon linux jenkins installation:
+
+sudo amazon-linux-extras install java-openjdk11 -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
